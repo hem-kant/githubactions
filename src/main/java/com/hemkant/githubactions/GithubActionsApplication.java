9feb4fb76c1b7b6ccbe437015c19ccb86b3ee44e
Hem Kant <hemkant.india@gmail.com>
2022-10-11 12:39:59 +0530

    Dummy endpoint

--- src/main/java/com/hemkant/githubactions/GithubActionsApplication.java
+++ src/main/java/com/hemkant/githubactions/GithubActionsApplication.java
@@ -2,10 +2,17 @@ package com.hemkant.githubactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubActionsApplication {
 
+	@GetMapping("/hello")
+	public  String HelloWorld(){
+		return "Hello World";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsApplication.class, args);
 	}
